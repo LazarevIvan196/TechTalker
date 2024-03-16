50c097e3d03ea5af0e0437ea649b147318c5df28
LazarevIvan196 <Gluk.666666@gmail.com>
2024-03-16 17:07:25 +0100

    beginning!

---
+++ src/test/java/com/example/techtalker/TechTalkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.techtalker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechTalkerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
